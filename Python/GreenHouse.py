df70b60471d19fe65592961791f20dd08caf4324
TimGabriel1 <zinwotatimothy@gmail.com>
2021-08-24 08:01:00 +0100

    Implementing Socketio

--- Python/GreenHouse.py
+++ Python/GreenHouse.py
@@ -35,33 +35,5 @@ def updateTemperature():
         return "No Data", 400
 
 
-# @app.route("/temp")
-# def update():
-    
-
-   
-#     print(ser.in_waiting)
-    
-    
-#     if ser.in_waiting > 0:
-#         line = ser.readline().decode('utf-8').rstrip()
-#         humidity = line[0:5]
-#         temperature = line[6:11]
-#         lightintensity = line[12:15]
-#         soilwater = line[16:19]
-#         now = datetime.datetime.now()
-        
-#         # print(line)
-#         # print(temperature)
-#         # print(humidity)
-#         # print(lightintensity)
-#         # print(soilwater)
-#         # templateData = {temperature}
-#         templateData = {'time' : now.strftime('%m-%d-%y %H:%M:%S'),'Temperature' : temperature, 'Humidity' : humidity , 'Light Intensity' : lightintensity, 'Soil Water Level': soilwater, 'Status' : 200}
-#         return jsonify(templateData)
-        
-#     else: 
-#             return "No Data", 400
-        
 if __name__ == "__main__":
     app.run()
